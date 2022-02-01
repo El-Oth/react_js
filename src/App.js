6810f8f346239ebc0991772f445a24c8b31f5e09
othmen2013 <othmenelamri@gmail.com>
2022-02-01 18:36:18 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  Forms from './Components/forms/form';
+
+function App() {
+  return (
+    <div>
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
